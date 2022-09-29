cf3f6325db11a292bf115bbb924cc85e1190efab
OgzSltkBgr <osbtoklucu@gmail.com>
2022-09-29 23:23:57 +0300

    2909

---
+++ src/test/java/pages/HotelMyCampPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class HotelMyCampPage {
+
+    public HotelMyCampPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[text()='Log in']")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(xpath = "//input[@id='UserName']")
+    public WebElement userNameBox;
+
+    @FindBy(xpath = "//input[@id='Password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@id='btnSubmit']")
+    public WebElement loginButonu;
+
+    @FindBy(xpath = "//span[@class='caption-subject font-green-sharp bold uppercase']")
+    public WebElement basariliGirisYazisiElementi;
+}
---
+++ src/test/java/tests/day18/C03_ConfigReader.java
@@ -0,0 +1,32 @@
+package tests.day18;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HotelMyCampPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C03_ConfigReader {
+    @Test
+    public void test01() {
+        // 1) com.techproed altinda bir package olustur : smoketest
+        //2) Bir Class olustur : PositiveTest
+        //3) Bir test method olustur positiveLoginTest()
+        //https://www.hotelmycamp.com adresine git
+        Driver.getDriver().get(ConfigReader.getProperty("hotelMyCampUrl"));
+
+        //login butonuna bas
+        HotelMyCampPage hotelMyCampPage= new HotelMyCampPage();
+        hotelMyCampPage.ilkLoginLinki.click();
+
+        //test data username: manager ,
+        //test data password : Manager1!
+        hotelMyCampPage.userNameBox.sendKeys(ConfigReader.getProperty("hotelMyCampUserName"));
+        hotelMyCampPage.passwordBox.sendKeys(ConfigReader.getProperty("hotelMyCampPassword"));
+        hotelMyCampPage.loginButonu.click();
+
+        //Degerleri girildiginde sayfaya basarili sekilde girilebildigini test et
+        Assert.assertTrue(hotelMyCampPage.basariliGirisYazisiElementi.isDisplayed());
+
+    }
+}
